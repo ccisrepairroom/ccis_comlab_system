76ea2ff006c0795c5aa65b2e87f77d20f834ba39
ccisrepairroom <ccisrepairroom@gmail.com>
2025-02-04 10:48:50 +0800

    view update edit delete is now inlined

--- app/Filament/Resources/EquipmentResource.php
+++ app/Filament/Resources/EquipmentResource.php
@@ -565,10 +565,12 @@ class EquipmentResource extends Resource
                     return Pages\ViewEquipment::getUrl([$record->id]);
                 })*/
                 ->actions([
-                    /*Tables\Actions\ViewAction::make('view_monitoring')
-                    ->label('View Equipment Records')
-                    ->icon('heroicon-o-presentation-chart-line')
+                    Tables\Actions\Action::make('view_monitoring')
+                    ->label('View ')
+                    ->icon('fas-eye')
                     ->color('info')
+                    ->modalSubmitAction(false)
+                    ->modalCancelAction(false)
                     ->modalHeading('Monitoring Records')
                     ->modalContent(function ($record) {
                         $equipmentId = $record->id;
@@ -578,10 +580,10 @@ class EquipmentResource extends Resource
                         return view('filament.resources.equipment-monitoring-modal', [
                             'monitorings' => $monitorings,
                         ]);
-                    }),*/
+                    }),
 
 
-                            Tables\Actions\Action::make('Update Status')
+                            Tables\Actions\Action::make('Update')
                             ->icon('heroicon-o-plus')
                             ->color('info')
                             ->requiresConfirmation()
