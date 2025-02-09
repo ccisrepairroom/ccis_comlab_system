dbfb32e02480a28c9ea20d6de39e78850f2ffc47
ccisrepairroom <ccisrepairroom@gmail.com>
2025-02-08 17:23:16 -0800

    print and downlaod button in facility mon modal

--- resources/views/filament/resources/facility-monitoring-modal.blade.php
+++ resources/views/filament/resources/facility-monitoring-modal.blade.php
@@ -16,6 +16,7 @@
                 </tr>
             </thead>
             <tbody>
+
             @foreach($monitorings->sortByDesc('created_at') as $monitoring)
             <tr class="hover:bg-gray-50 transition duration-200">
                 <td class="border border-gray-300 px-6 py-4">{{ $monitoring->user->name ?? 'Unknown' }}</td>
@@ -24,6 +25,17 @@
                     </tr>
                 @endforeach
             </tbody>
-        </table>
+        </table>  
     @endif
 </div>
+<div class="flex items-center space-x-1 ml-10">
+    <button type="button" class="focus:outline-none text-white bg-orange-500 hover:bg-orange-600 focus:ring-4 focus:ring-orange-500 font-medium rounded-lg text-sm px-5  py-2.5 me-2 mb-2">
+        Print
+    </button>
+    <button type="button" class="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">Download</button>
+
+</div>
+
+</div>
+
+
