ed09ee9f186201f115df6931a2e4f3d6e226b04e
ccisrepairroom <ccisrepairroom@gmail.com>
2025-02-17 15:51:03 +0800

    .

--- resources/views/livewire/home-page.blade.php
+++ resources/views/livewire/home-page.blade.php
@@ -134,37 +134,37 @@
             </div>
 
             
-
-<div id="animation-carousel" class="relative w-full" data-carousel="static">
-    <!-- Carousel wrapper -->
-    <div class="relative h-56 overflow-hidden rounded-lg md:h-96 object-contain">
-         <!-- Item 1 -->
-         @foreach($equip->alternate_images as $image)
-        <div class="hidden duration-100 ease-linear" data-carousel-item>
-            <img src="{{ url('storage', $image) }}" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
-        </div>
-      
-        @endforeach
-    </div>
-   <!-- Slider controls -->
-    <button type="button" class="absolute top-0 start-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-prev>
-        <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-orange-500/30 dark:bg-orange-500/30 group-hover:bg-orange-500/50 dark:group-hover:bg-orange-500/60 group-focus:ring-4 group-focus:ring-orange-500 dark:group-focus:ring-orange-500/70 group-focus:outline-none">
-            <svg class="w-4 h-4 text-white dark:text-orange-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 1 1 5l4 4"/>
-            </svg>
-            <span class="sr-only">Previous</span>
-        </span>
-    </button>
-    <button type="button" class="absolute top-0 end-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-next>
-        <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-orange-500/30 dark:bg-orange-500/30 group-hover:bg-orange-500/50 dark:group-hover:bg-orange-500/60 group-focus:ring-4 group-focus:ring-orange-500 dark:group-focus:ring-orange-500/70 group-focus:outline-none">
-            <svg class="w-4 h-4 text-white dark:text-orange-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4"/>
-            </svg>
-            <span class="sr-only">Next</span>
-        </span>
-    </button>
-
-</div>
+            @if($equip->alternate_images && count($equip->alternate_images) > 0)
+            <div id="animation-carousel" class="relative w-full" data-carousel="static">
+                <!-- Carousel wrapper -->
+                <div class="relative h-56 overflow-hidden rounded-lg md:h-96 object-contain">
+                    <!-- Item 1 -->
+                      
+                    @foreach($equip->alternate_images as $image)
+                    <div class="hidden duration-100 ease-linear" data-carousel-item>
+                        <img src="{{ url('storage', $image) }}" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
+                    </div>
+                    @endforeach
+                </div>
+              <!-- Slider controls -->
+                <button type="button" class="absolute top-0 start-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-prev>
+                    <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-orange-500/30 dark:bg-orange-500/30 group-hover:bg-orange-500/50 dark:group-hover:bg-orange-500/60 group-focus:ring-4 group-focus:ring-orange-500 dark:group-focus:ring-orange-500/70 group-focus:outline-none">
+                        <svg class="w-4 h-4 text-white dark:text-orange-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 1 1 5l4 4"/>
+                        </svg>
+                        <span class="sr-only">Previous</span>
+                    </span>
+                </button>
+                <button type="button" class="absolute top-0 end-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-next>
+                    <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-orange-500/30 dark:bg-orange-500/30 group-hover:bg-orange-500/50 dark:group-hover:bg-orange-500/60 group-focus:ring-4 group-focus:ring-orange-500 dark:group-focus:ring-orange-500/70 group-focus:outline-none">
+                        <svg class="w-4 h-4 text-white dark:text-orange-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4"/>
+                        </svg>
+                        <span class="sr-only">Next</span>
+                    </span>
+                </button>
+            </div>
+            @endif
 
             <!-- Modal body -->
             <div class="p-4 md:p-5 space-y-4">
