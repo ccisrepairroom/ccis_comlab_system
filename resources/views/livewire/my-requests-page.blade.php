54101791116b485cffdcdcaaa696442d1fe34825
clairenakilacc <clairenakilacc@gmail.com>
2025-05-04 21:09:29 +0800

    backdrop white

--- resources/views/livewire/my-requests-page.blade.php
+++ resources/views/livewire/my-requests-page.blade.php
@@ -96,26 +96,22 @@
    <!-- Modal -->
 <div 
   x-show="open"
-  x-transition
   style="display: none"
-  class="fixed inset-0 flex items-center justify-center backdrop-brightness-75 z-50"
+  class="fixed inset-0 flex items-start justify-center backdrop-brightness-75 z-50"
   x-cloak
 >
 
-    <div class="relative p-4 w-full max-w-2xl max-h-full mt-16">
-      <div class="my-10 bg-white p-6 rounded shadow-lg max-h-[80vh] overflow-y-auto">
-        <h3 class="text-xl font-semibold" x-text="'Request Code: ' + (selectedRequest?.code ?? '')"></h3>
-        <p class="text-gray-600 text-sm pt-5">
-          <strong>Borrower:</strong> <span x-text="selectedRequest?.borrowed_by"></span><br>
-          <strong>Remarks:</strong><br>
-          <span x-text="selectedRequest?.remarks"></span>
-        </p>  
-        <div class="flex justify-end">
-          <button class="mt-4 px-4 py-2 bg-orange-500 text-white rounded" @click="open = false">Close</button>
-        </div> 
-      </div>
+  <div class="relative p-4 w-full max-w-[300mm] max-h-[400mm] top-0">
+    <div class="my-10 bg-white p-6 rounded shadow-lg max-h-full overflow-y-auto">
+      <h3 class="text-xl font-semibold" x-text="'Request Code: ' + (selectedRequest?.code ?? '')"></h3>
+      <p class="text-gray-600 text-sm pt-5">
+        <strong>Borrower:</strong> <span x-text="selectedRequest?.borrowed_by"></span><br>
+        <strong>Remarks:</strong><br>
+        <span x-text="selectedRequest?.remarks"></span>
+      </p>  
+      <div class="flex justify-end">
+        <button class="mt-4 px-4 py-2 bg-orange-500 text-white rounded" @click="open = false">Close</button>
+      </div> 
     </div>
   </div>
 </div>
-</div>
-
