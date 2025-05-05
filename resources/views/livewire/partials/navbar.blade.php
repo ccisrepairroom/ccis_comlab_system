c68a5abba39bde378493c477d1724aae33d3794a
ccisrepairroom <ccisrepairroom@gmail.com>
2025-05-05 14:25:48 +0800

    .

--- resources/views/livewire/partials/navbar.blade.php
+++ resources/views/livewire/partials/navbar.blade.php
@@ -71,7 +71,7 @@
 
 
            @auth
-           <div class="hs-dropdown [--strategy:static] md:[--strategy:fixed] [--adaptive:none] md:[--trigger:hover] md:py-4">
+           <div class="hs-dropdown [--strategy:static] md:[--strategy:fixed] [--adaptive:none] [--trigger:click] md:py-4">
               <button type="button" class="flex items-center w-full text-gray-500 hover:text-gray-400 font-medium dark:text-gray-400 dark:hover:text-gray-500">
                 {{auth()->user()->name}}
             <svg class="ms-2 w-4 h-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
