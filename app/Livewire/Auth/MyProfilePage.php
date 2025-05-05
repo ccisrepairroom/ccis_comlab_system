d5322c2ee4e87741c249e1bb9817f65044e5face
ccisrepairroom <ccisrepairroom@gmail.com>
2025-05-05 16:22:39 +0800

    .

--- app/Livewire/Auth/MyProfilePage.php
+++ app/Livewire/Auth/MyProfilePage.php
@@ -3,7 +3,9 @@
 namespace App\Livewire\Auth;
 
 use Livewire\Component;
+use Livewire\Attributes\Title;
 
+#[Title('My Profile - CCIS ERMA')]
 class MyProfilePage extends Component
 {
     public function render()
