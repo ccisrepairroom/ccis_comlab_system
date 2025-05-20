11ca8a0188a0535231ebd363ee0ce6649806aefe
ccisrepairroom <ccisrepairroom@gmail.com>
2025-05-20 19:32:43 +0800

    .

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,11 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+    server: {
+        host: '192.168.128.149', // ✅ RDC-accessible IP
+        port: 5173,               // optional, default is 5173
+        strictPort: true,
+    },
     plugins: [
         laravel({
             input: [
